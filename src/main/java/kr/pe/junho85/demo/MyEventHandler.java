43e9456167f34726c96687583df6fea3f021ed30
june.kim <junho85@gmail.com>
2020-06-22 17:37:18 +0900

    @EnableAsync

--- src/main/java/kr/pe/junho85/demo/MyEventHandler.java
+++ src/main/java/kr/pe/junho85/demo/MyEventHandler.java
@@ -1,14 +1,38 @@
 package kr.pe.junho85.demo;
 
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MyEventHandler {
 
     @EventListener
+//    @Order(Ordered.HIGHEST_PRECEDENCE)
+    @Async
     public void handle(MyEvent myEvent) {
+        System.out.println(Thread.currentThread().toString());
         System.out.println("이벤트 받았다. 데이터는 " + myEvent.getData());
     }
+
+    @EventListener
+    @Async
+    public void handle(ContextRefreshedEvent event) {
+        System.out.println(Thread.currentThread().toString());
+        System.out.println("ContextRefreshedEvent");
+    }
+
+    @EventListener
+    @Async
+    public void handle(ContextClosedEvent event) {
+        System.out.println(Thread.currentThread().toString());
+        System.out.println("ContextClosedEvent");
+    }
+
+
 }
